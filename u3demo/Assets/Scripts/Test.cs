467aa56ffa69ef83e5fa1b79b4b9aa52650ed6cf
chengqian1521 <chengqian1521@qq.com>
2022-12-11 22:39:33 +0800

    增加文本显示控件

--- u3demo/Assets/Scripts/Test.cs
+++ u3demo/Assets/Scripts/Test.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Threading;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
@@ -15,16 +18,33 @@ public class Test : MonoBehaviour
         onConenctedFlag = true;
     }
 
+    private static SynchronizationContext mainThreadSyncContext;
+
     static public void OnDataFunc(string text)
     {
         // 从客户端接受数据的回调，注意此回调不是在unity主线程中，如需操作需post主线程中。
         Debug.Log("on recv data from client:" + text);
-
+        mainThreadSyncContext.Post(_ =>
+        {
+            // This code here will run on the main thread
+            GameObject go = GameObject.Find("Canvas/Text");
+            if(go != null)
+            {
+                Text ta = go.GetComponent<Text>();
+                if(ta != null)
+                 ta.text = "从客户端接收到:" + text;
+            }
+                 
+            
+        }, null);
     }
 
     // Start is called before the first frame update
     void Start()
     {
+        Application.targetFrameRate = 60;
+
+        mainThreadSyncContext = SynchronizationContext.Current;
         // 注册
         DolitDataChannel.onConnectedCallback = OnConenctedFunc;
         DolitDataChannel.onTextDataCallback = OnDataFunc;
