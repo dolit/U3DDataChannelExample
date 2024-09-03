d549646a73a13c83e35380d99ae1684070af7821
chengqian <chengqian1521@qq.com>
2024-09-03 15:54:32 +0800

    update mic pull websocket.optimize cpu usage.

--- u3demo/Assets/Scripts/DLCAMicorphoneChannel.cs
+++ u3demo/Assets/Scripts/DLCAMicorphoneChannel.cs
@@ -1,19 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityWebSocket;
 using UnityEngine.UI;
+using NativeWebSocket;
 
 // 此文件为获取访问端网页的麦克风数据的Demo示例。
 // 仅需找一个全局唯一的对象示例附加上此脚本即可。
 
 public class DLCAMicorphoneChannel : MonoBehaviour
 {
+    private WebSocket websocket;
+
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
+        Application.targetFrameRate = 60;
+
         int port = DolitDataChannel.GetPort();
-        if(port <= 0)
+        port = 3003;
+        if (port <= 0)
         {
             Debug.Log("dlca port is zero,please cheack enabed data chennel from cms app settings.");
             return;
@@ -21,60 +24,50 @@ public class DLCAMicorphoneChannel : MonoBehaviour
         // 创建实例
         string address = "ws://127.0.0.1:" + port  + "/pull_mic";
         websocket = new WebSocket(address);
-
-        // 注册回调
-        websocket.OnOpen += OnOpen;
-        websocket.OnClose += OnClose;
-        websocket.OnMessage += OnMessage;
-        websocket.OnError += OnError;
-
-        // 连接
-        websocket.ConnectAsync();
-    }
-
-    void OnOpen(object sender, OpenEventArgs openArgs)
-    {
-
-    }
-
-
-    private void OnMessage(object sender, MessageEventArgs e)
-    {
-        // 麦克风数据先触发一帧Text Json数据来描述音频格式，然后紧接着会触发一帧 Binary为实际语音的pcm
-        if (e.IsBinary)
+        websocket.OnOpen += () =>
         {
-            // 此处为接收到的pcm数据。
-            Debug.Log(string.Format("Receive Pcm Bytes Len: {0}", e.RawData.Length));
-        }
-        else if (e.IsText)
-        {
-            Debug.Log(string.Format("Receive Pcm Format: {0}", e.Data));
+            Debug.Log("DLCAMicorphoneChannel Connection open!");
+        };
 
-            GameObject go = GameObject.Find("Canvas/MicText");
-            if (go != null)
+        websocket.OnError += (e) =>
+        {
+            Debug.Log("DLCAMicorphoneChannel Error! " + e);
+        };
+
+        websocket.OnClose += (e) =>
+        {
+            Debug.Log("DLCAMicorphoneChannel Connection closed!");
+        };
+
+        websocket.OnMessage += (bytes) =>
+        {
+            // bytes.
+            if (bytes.Length > 200)
             {
-                Text ta = go.GetComponent<Text>();
-                if (ta != null)
-                    ta.text = "接收到来自客户端语音数据 格式:" + e.Data;
+                // 此处为接收到的pcm数据。
+                Debug.Log(string.Format("DLCAMicorphoneChannel Receive Pcm Bytes Len: {0}", bytes.Length));
+
+            } else {
+                string message = System.Text.Encoding.UTF8.GetString(bytes);
+                Debug.Log(string.Format("DLCAMicorphoneChannel Receive Pcm Format: {0}", message));
+                GameObject go = GameObject.Find("Canvas/MicText");
+                if (go != null)
+                {
+                    Text ta = go.GetComponent<Text>();
+                    if (ta != null)
+                        ta.text = "接收到来自客户端语音数据 格式:" + message;
+                }
             }
-        }
+
+        };
+        await websocket.Connect();
+        Debug.Log("DLCAMicorphoneChannel connnecting.");
     }
 
-    private void OnClose(object sender, CloseEventArgs e)
-    {
-       // AddLog(string.Format("Closed: StatusCode: {0}, Reason: {1}", e.StatusCode, e.Reason));
-    }
-
-    private void OnError(object sender, ErrorEventArgs e)
-    {
-       // AddLog(string.Format("Error: {0}", e.Message));
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+#if !UNITY_WEBGL || UNITY_EDITOR
+        websocket.DispatchMessageQueue();
+#endif
     }
-
-    private WebSocket websocket;
 }
